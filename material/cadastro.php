11b3b7a6c0702fd14193b3185798ee2176267522
Matheus Guimarães <100164786+garotodeti@users.noreply.github.com>
2022-11-04 20:29:36 -0300

    Adicionando o conteúdo em PHP

---
+++ material/cadastro.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="UTF-8" />
+	  <meta http-equiv="refresh" content="">
+	  <title>Formulário de Login e Registro com HTML5 e CSS3</title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	  <link rel="stylesheet" type="text/css" href="login.css" />
+	</head>
+
+	<body>
+		<img src="r.jpg" alt="">
+			<div class="container">
+
+				<a class="links" id="paracadastro"></a>
+				<a class="links" id="paralogin"></a>
+		  <?php
+			  //Iniciar  Sessão
+			  //session_start();
+				
+			  if(isset($_POST['Cadastrar'])):
+				echo "<p>entrou!</p>";	
+				
+				//Conexão
+				require_once 'conexaoBanco.php';
+
+				$nome=mysqli_escape_string($connect,$_POST['nome_cad']);
+				$sobrenome=mysqli_escape_string($connect,$_POST['Snome_cad']);
+				$CPF=mysqli_escape_string($connect,$_POST['CPF_cad']);
+				$email=mysqli_escape_string($connect,$_POST['email_cad']);
+				$senha=mysqli_escape_string($connect,$_POST['senha_cad']);
+				$Rsenha=mysqli_escape_string($connect,$_POST['Rsenha_cad']);
+				$endereco=mysqli_escape_string($connect,$_POST['End_cad']);
+				$dataDeNascimento=mysqli_escape_string($connect,$_POST['Dat_cad']);
+				
+		
+				$sql= "INSERT INTO usuario(nomUser,dateNascm,dscEmail,numCPF,sobrenome,dscRaschSenhas,endereco) VALUES ('$nome','$dataDeNascimento','$email','$CPF','$sobrenome','$senha','$endereco')";
+				
+				if(mysqli_query($connect,$sql)):
+				  echo "Sucesso";
+				else:
+				  echo "Erro !";		
+				endif;
+				
+			  endif;	
+			
+		  ?>
+
+
+		  <!--FORMULÁRIO DE CADASTRO-->
+		<div class="container">
+		 
+		<div class="content">      
+		  <div id="cadastro">
+
+
+			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> 
+			  <h1>Cadastro</h1> 
+			   
+			  <p> 
+				<label for="nome_cad">Nome</label>
+				<input id="nome_cad" name="nome_cad" required="required" type="name" placeholder="nome" />
+			  </p>
+			   
+				  <p> 
+				<label for="Snome_cad">Sobrenome</label>
+				<input id="Snome_cad" name="Snome_cad" required="required" type="name" placeholder="nome" />
+			  </p> 
+			  
+				  <p> 
+				<label for="CPF_cad">CPF</label>
+				<input id="CPF_cad" name="CPF_cad" required="required" type="text" placeholder="000.000.000-00"/>
+			   
+			  <p> 
+				<label for="email_cad">E-mail</label>
+				<input id="email_cad" name="email_cad" required="required" type="text" placeholder="contato@gmail.com"/> 
+			  </p>
+			   
+			  <p> 
+				<label for="senha_cad">Senha</label>
+				<input id="senha_cad" name="senha_cad" required="required onchange" type="password" placeholder="No mínimo 8 caracteres, 1 letra minúscula, 1 maiúscula e 1 número" />
+			  </p>
+
+			  <p> 
+				<label for="Rsenha_cad">Repita a senha</label>
+				<input id="Rsenha_cad" name="Rsenha_cad" required="required onchange" type="password" placeholder="No mínimo 8 caracteres, 1 letra minúscula, 1 maiúscula e 1 número"/>
+			  </p>	
+				
+			  <p> 
+				<label for="End_cad">Endereço</label>
+				<input id="End_cad" name="End_cad" required="required" type="text" placeholder="ex. rua, número, cidade, estado"/>
+			  </p>
+			  
+			  <p> 
+				<label for="Dat_cad">Data de Nascimento</label>
+				<input id="Dat_cad" name="Dat_cad" required="required" type="date" placeholder="00/00/0000"/>
+			  </p> 
+			  
+			  <p>             
+				<button type="submit" name="Cadastrar"> Enviar </button>
+			  </p>
+			   
+			  <p class="link">  
+				Já tem conta?
+				<a href="login.php"> Ir para Login </a>
+			  </p>
+			</form>
+			
+		  </div>
+		
+		</div>
+		</div>
+	</body>
+</html>
---
+++ material/telaPrincipal.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8" content="text/html">
+    <meta name="keywords" content="Faça viagens mais rápido.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="general.css">
+    <link rel="stylesheet" href="reset.css">
+    <title>Home</title
+  </head>
+  <?php
+session_start();
+
+//include('verificarLogin.php');
+
+?>
+  <body>
+    <!-- Header Banner -->
+    <header>
+      <!-- Headline: Container que representa o conteúdo de Header -->
+      <div class="headline">
+        <!-- headcast: Agrupamento dos conteúdos de Headline para serem manipulados de forma
+        separadas pelo CSS -->
+        <div class="headcast">
+          <div class="brandline">
+            <div class="brand">
+              <h1 id="label">Transport</h1>
+            </div>
+
+            <div class="profile">
+              <div class="hatch">
+                <div class="pic">
+                  <button id="profile-menu-agent" type="button">
+                    <img id="miniature" src="profile.jpg" alt="Profile picture" width="35" height="35">
+                  </button>
+                </div>
+                <h2>
+                  <?php
+                    echo $_SESSION['nome'];
+                  ?>
+                </h2>
+                <a href=logout.php>Sair</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main>
+      <article class="content">
+        <aside class="trip-details-planner">
+          <div class="trip-details-conditions">
+            <div class="trip-conditions">
+              <div class="label-placing">
+                <h1 class="label">Plano de Viagem</h1>
+              </div>
+              <div class="search">
+
+                <form class="travel-plan-letter" action="index.html" method="post">
+
+                  <label for="trip-start-condition">Partida</label>
+                  <input id="trip-start-condition" type="search" name="search" placeholder="Qual o seu endereço de origem?">
+
+                  <label for="trip-arrival-condition">Destino</label>
+                  <input id="trip-arrival-condition" type="search" name="search" placeholder="Pra onde você quer ir?" >
+
+                  <input id="submit-trip" type="submit" name="search" value="confirmar">
+
+                  <input id="cancel-trip" type="button" name="cancel" value="cancelar">
+                </form>
+              </div>
+            </div>
+          </div>
+        </aside>
+
+        <section class="placeholder-map-area">
+          <div class="map-placer">
+
+          </div>
+        </section>
+      </article>
+    </main>
+  </body>
+</html>
