d43da7b9c0dc1f0289dabdc18f6d36e4edffefbb
Matheus Guimarães <100164786+garotodeti@users.noreply.github.com>
2022-11-25 17:05:33 -0300

    Mat (#4)
    * update em sair
    
    * Atualizações
    
    Tela de exportação, sair, voltar

---
+++ material/processaPontos.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+//Incluir a conexao com BD
+include_once("conexaoBanco.php");
+
+//Receber os dados do formulário
+//$arquivo = $_FILES['arquivo'];
+//var_dump($arquivo);
+$arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+
+//ler todo o arquivo para um array
+$dados = file($arquivo_tmp);
+//var_dump($dados);
+
+foreach($dados as $linha){
+	$linha = trim($linha);
+	$valor = explode(' ', $linha);
+	var_dump($valor);
+	
+	$idPontos = $valor[0];
+	$valLatitude = $valor[1];
+	$valLongitude = $valor[2];
+	$nomPonto = $valor[3];
+	$valPosMetr = $valor[4];
+	
+	$sql = "INSERT INTO pontos(idPontos, valLatitude, valLongitude, nomPonto, valPosMetr) VALUES('$idPontos', '$nomPonto','$valPosMetr', '$valLatitude', '$valLongitude')";
+	
+	if(mysqli_query($connect,$sql)):
+		echo "Sucesso";
+	  else:
+		echo "Erro !";		
+	  endif;
+	  
+}
+$_SESSION['msg'] = "<p style='color: green;'>Carregado os dados com sucesso!</p>";
+header("Location: TelaExportacaoDados.php");
+
+
+
